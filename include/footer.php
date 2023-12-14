3e4030da93fa614b4800a8a8c465ec34ad8df5f3
@wil3010 <wilsona_loaiza@soy.sena.edu.co>
2023-12-13 21:16:48 -0500

    primer version

---
+++ include/footer.php
@@ -0,0 +1,8 @@
+	
+<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2023 Control de punto de venta - <a href="index.php">PharmaPoscontrol-control</a> </b> Todos los derechos reservados.
+		</div>
+	</div>
---
+++ include/sidebar.php
@@ -0,0 +1,184 @@
+<div class="span3">
+					<div class="sidebar">
+					  <ul class="widget widget-menu unstyled">
+						<li><a class="collapsed" data-toggle="collapse" href="#togglePages4"><i class="menu-icon icon-cog"></i>
+									<i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right"></i>Administrar Clientes </a>
+						  <ul id="togglePages4" class="collapse unstyled">
+							  
+						    <li> <a href="registrar_cliente.php"> <i class="icon-tasks"></i> Agregar Cliente </a> </li>
+						    <li> <a href="administrar_clientes.php"> <i class="icon-tasks"></i> Administrar Clientes</a></li>
+					      </ul>
+	  </li>						
+					<li>
+								<a class="collapsed" data-toggle="collapse" href="#togglePages3">
+									<i class="menu-icon icon-cog"></i>
+									<i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right"></i>
+									Administrar Productos
+								</a>
+							  <ul id="togglePages3" class="collapse unstyled">
+									<li>
+										<a href="registrar_producto.php">
+											<i class="icon-tasks"></i>
+											Agregar Producto										</a>
+								  </li>
+									<li>
+										<a href="administrar_productos.php">
+											<i class="icon-tasks"></i>
+									Administrar Producto</a></li>
+								</ul>
+							</li>
+					<li>
+						<a class="collapsed" data-toggle="collapse" href="#togglePages3">
+									<i class="menu-icon icon-cog"></i>
+									<i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right"></i>
+									Administrar Proveedores
+								</a>
+							  <ul id="togglePages3" class="collapse unstyled">
+									<li>
+										<a href="">
+											<i class="icon-tasks"></i>
+											Agregar Proveedor		</a>
+								  </li>
+									<li>
+										<a href="">
+											<i class="icon-tasks"></i>
+									Administrar Proveedor</a></li>
+								</ul>
+							</li>
+					<li>
+					<a class="collapsed" data-toggle="collapse" href="#togglePages3">
+									<i class="menu-icon icon-cog"></i>
+									<i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right"></i>
+									Administrar Inventario
+								</a>
+							  <ul id="togglePages3" class="collapse unstyled">
+									<li>
+										<a href="">
+											<i class="icon-tasks"></i>
+											Agregar										</a>
+								  </li>
+									<li>
+										<a href="">
+											<i class="icon-tasks"></i>
+									Administrar Inventario</a></li>
+								</ul>
+							</li>
+					<li>
+					<a class="collapsed" data-toggle="collapse" href="#togglePages3">
+									<i class="menu-icon icon-cog"></i>
+									<i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right"></i>
+									Administrar Usuarios
+								</a>
+							  <ul id="togglePages3" class="collapse unstyled">
+									<li>
+										<a href="">
+											<i class="icon-tasks"></i>
+											Agregar Usuario										</a>
+								  </li>
+									<li>
+										<a href="">
+											<i class="icon-tasks"></i>
+									Administrar Usuarios</a></li>
+								</ul>
+							</li>
+					<li>
+					<a class="collapsed" data-toggle="collapse" href="#togglePages3">
+									<i class="menu-icon icon-cog"></i>
+									<i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right"></i>
+									Administracion
+								</a>
+							  <ul id="togglePages3" class="collapse unstyled">
+									<li>
+										<a href="">
+											<i class="icon-tasks"></i>
+											Agregar										</a>
+								  </li>
+									<li>
+										<a href="">
+											<i class="icon-tasks"></i>
+									Administrar </a></li>
+								</ul>
+							</li>
+					<li>
+					<a class="collapsed" data-toggle="collapse" href="#togglePages3">
+									<i class="menu-icon icon-cog"></i>
+									<i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right"></i>
+									Compras
+								</a>
+							  <ul id="togglePages3" class="collapse unstyled">
+									<li>
+										<a href="">
+											<i class="icon-tasks"></i>
+											Agregar compra										</a>
+								  </li>
+									<li>
+										<a href="">
+											<i class="icon-tasks"></i>
+									Administrar Compras</a></li>
+								</ul>
+							</li>
+					<li>
+					<a class="collapsed" data-toggle="collapse" href="#togglePages3">
+									<i class="menu-icon icon-cog"></i>
+									<i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right"></i>
+									Factura
+								</a>
+							  <ul id="togglePages3" class="collapse unstyled">
+									<li>
+										<a href="">
+											<i class="icon-tasks"></i>
+											Agregar factura									</a>
+								  </li>
+									<li>
+										<a href="">
+											<i class="icon-tasks"></i>
+									Administrar factura</a></li>
+								</ul>
+							</li>
+					<li>
+								<a class="collapsed" data-toggle="collapse" href="#togglePages1">
+									<i class="menu-icon icon-cog"></i>
+									<i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right"></i>
+									Administrar Categorias
+								</a>
+					            <ul id="togglePages1" class="collapse unstyled">
+									<li>
+										<a href="agregar_categoria.php">
+											<i class="icon-tasks"></i>
+											Agregar categoria									</a>
+			</li>
+									<li>
+										<a href="administrar_categoria.php">
+											<i class="icon-tasks"></i>
+									Categorias Almacenadas</a></li>
+							
+								  
+								</ul>
+							</li>
+						</ul>
+						<ul class="widget widget-menu unstyled">
+							<li>
+								<a class="collapsed" data-toggle="collapse" href="#togglePages2">
+									<i class="menu-icon icon-cog"></i>
+									<i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right"></i>
+									Administrar SubCategorias
+								</a>
+							    <ul id="togglePages2" class="collapse unstyled">
+									<li>
+										<a href="agregar_subcategoria.php">
+											<i class="icon-tasks"></i>
+											Agregar SubCategoria	</a>
+								  </li>
+									<li>
+										<a href="administrar_subcategoria.php">
+											<i class="icon-tasks"></i>
+									SubCategorias Almacenadas</a></li>
+								</ul>
+							</li>
+</ul>
+
+
+						
+
+					</div><!--/.sidebar-->
+				</div><!--/.span3-->
